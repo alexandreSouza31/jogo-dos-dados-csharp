cffd87d95a0306b406f8f231bafa0b71a82e2d84
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-04-01 22:18:59 -0300

    refact: ajustará o método Rolar dado

--- jogoDadosConsoleApp/Program.cs
+++ jogoDadosConsoleApp/Program.cs
@@ -10,28 +10,51 @@ namespace jogoDados.ConsoleApp
             while (true)
             {
                 int posicaoUsuario = 0;
+                int posicaoComputador = 0;
 
                 bool jogoEmAndamento = true;
+                
+                Console.Write("Digite o seu nome de Usuário: ");
+                string nomeJogador = Console.ReadLine()!;
 
                 while (jogoEmAndamento) 
                 {
+                    Console.Clear();
                     ExibirCabeçalho();
 
-                    int resultado = RolarDado();
+                    var (resultadoUsuario, resultadoComputador )= RolarDado(nomeJogador);
 
-                    ExibirResultado(resultado);
-
-                    posicaoUsuario += resultado;
+                    posicaoUsuario += resultadoUsuario;
 
 
                     if (posicaoUsuario >= limiteLinhaChegada)
                     {
-                        Console.WriteLine("Parabéns! Você alcançou a linha de chegada!");
-                        jogoEmAndamento= false;
-                    }
-                    else Console.WriteLine($"O jogador está na posição: {posicaoUsuario} de {limiteLinhaChegada}");
+                        Console.WriteLine("\nParabéns! Você alcançou a linha de chegada!");
+                        Console.ReadLine();
+                        jogoEmAndamento = false;
 
-                    Console.WriteLine("Digite Enter para continuar...");
+                        continue;
+                    }
+                    else Console.WriteLine($"\nO jogador está na posição: {posicaoUsuario} de {limiteLinhaChegada}.");
+
+                    Console.Write("\nDigite Enter para continuar...");
+                    Console.ReadLine();
+
+                    posicaoComputador += resultadoComputador;
+
+
+                    if (posicaoComputador >= limiteLinhaChegada)
+                    {
+                        Console.WriteLine("\nQue pena o Computador alcançou a linha de chegada!");
+                        Console.ReadLine() ;
+                        jogoEmAndamento = false;
+
+                        continue;
+                    }
+                    else Console.WriteLine($"\nO Computador está na posição: {posicaoComputador} de {limiteLinhaChegada}.");
+                    Console.WriteLine("------------------------------");
+
+                    Console.WriteLine("\nDigite Enter para continuar...");
                     Console.ReadLine();
                 }
 
@@ -44,32 +67,38 @@ namespace jogoDados.ConsoleApp
 
         static void ExibirCabeçalho()
         {
-            Console.Clear();
+            //Console.Clear();
             Console.WriteLine("------------------------------");
             Console.WriteLine("      Jogo  de Dados");
-            Console.WriteLine("------------------------------");
         }
 
-        static int RolarDado()
+        static (int resultadoUsuário,int resultadoComputador) RolarDado(string nomeJogador="")
         {
-            Console.Write("Pressione Enter para rolar o dado:");
-            Console.ReadLine();
+            bool vezJogador = true;
+
+            Console.WriteLine($"\n\nTurno do(a) {nomeJogador}:");
+
             Random geradorNumeros = new Random();
-            int resultado = geradorNumeros.Next(1, 7);
 
-            return resultado;
-        }
+            if (!string.IsNullOrEmpty(nomeJogador))
+            {
+                Console.Write("\n\nPressione Enter para rolar o dado:");
+                Console.ReadLine();
+                
+            }
+            int resultadoUsuario = geradorNumeros.Next(1, 7);
+            Console.WriteLine($"\n\nO valor sorteado para {nomeJogador} foi {resultadoUsuario}!");
 
-        static void ExibirResultado(int resultado)
-        {
+            vezJogador = false;
+            Console.WriteLine("\n\nTurno do Computador:");
+            int resultadoComputador = geradorNumeros.Next(1, 7);
+            Console.WriteLine($"\n\nO valor sorteado para o Computador foi {resultadoComputador}!");
 
-            Console.WriteLine("------------------------------");
-            Console.WriteLine($"O valor sorteado foi {resultado}");
-            Console.WriteLine("------------------------------");
+
+            return (resultadoUsuario,resultadoComputador);
         }
 
         static string ExibirMenuContinuar(){
-            Console.WriteLine("Hello, World!");
             Console.WriteLine("Deseja continuar? [s/n]");
             string opcaoContinuar = Console.ReadLine()!.ToUpper();//essa exclamação diz pro VS não reclamar
 
